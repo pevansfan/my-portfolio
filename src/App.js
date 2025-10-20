38a9281e12cd25e8fa6c8cde8a2cd6e2803a678f
Evans PARFAIT <fani.prft@gmail.com>
2025-10-20 02:31:36 +0200

    Mise à jour du portfolio React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import "./App.css";
