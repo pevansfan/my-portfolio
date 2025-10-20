bf83bf5caaef1e033fa49a1852d5511b0c4b0064
Evans PARFAIT <fani.prft@gmail.com>
2025-10-20 02:53:44 +0200

    Mise à jour du portfolio React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import "./App.css";
@@ -100,7 +100,7 @@ function App() {
   const [showSplash, setShowSplash] = useState(true);
 
   return (
-    <Router basename="/my-portfolio">
+    <Router>
       <CustomCursor />
       {/* <ScrollToTopButton /> */}
       {showSplash ? (
